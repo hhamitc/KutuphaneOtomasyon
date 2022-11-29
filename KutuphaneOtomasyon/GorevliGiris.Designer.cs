f77bd9020e39335dc0b5c3a0782becbf9e6732fc
hamit.canpolat <hamit.canpolat@SMPR303-09>
2022-11-29 18:19:19 +0300

    KitapSilmeEklendi2

--- KutuphaneOtomasyon/GorevliGiris.Designer.cs
+++ KutuphaneOtomasyon/GorevliGiris.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KutuphaneOtomasyon
 {
-    partial class Form1
+    partial class GorevliGiris
     {
         /// <summary>
         ///Gerekli tasarımcı değişkeni.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GorevliGiris));
             this.adGiristxt = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
--- KutuphaneOtomasyon/IslemPaneli.Designer.cs
+++ KutuphaneOtomasyon/IslemPaneli.Designer.cs
@@ -75,7 +75,7 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(234, 690);
+            this.panel1.Size = new System.Drawing.Size(234, 785);
             this.panel1.TabIndex = 0;
             // 
             // silTurbtn
@@ -341,12 +341,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1222, 690);
+            this.ClientSize = new System.Drawing.Size(1222, 785);
             this.Controls.Add(this.kullaniciBilgiPanel);
             this.Controls.Add(this.panel1);
             this.IsMdiContainer = true;
             this.Name = "IslemPaneli";
             this.Text = "IslemPaneli";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.IslemPaneli_Load);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
